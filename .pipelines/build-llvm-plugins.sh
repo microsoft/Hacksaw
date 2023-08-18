a77b41da9c6bd1b79896886d1bd4b68eade20cd7
Sangho Lee <Sangho.Lee@microsoft.com>
2023-08-18 14:47:09 -0700

    remove c code

--- .pipelines/build-llvm-plugins.sh
+++ .pipelines/build-llvm-plugins.sh
@@ -4,7 +4,12 @@
 
 set -x
 
-git clone --depth=1 -b release/15.x https://github.com/llvm/llvm-project.git ~/llvm-project
+# git clone --depth=1 -b release/15.x https://github.com/llvm/llvm-project.git ~/llvm-project
+
+pushd ~
+https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.0/llvm-project-15.0.0.src.tar.xz
+tar xf llvm-project-15.0.0.src.tar.xz 
+popd
 
 pushd gen_database
 pushd platform
