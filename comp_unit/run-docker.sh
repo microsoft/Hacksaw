153973c770e17fe330957dde37e9ec385a41c052
Sangho Lee <sangho.lee.cs@gmail.com>
2023-10-18 08:26:51 -0700

    comp_unit analysis with built-in-only objects

--- comp_unit/run-docker.sh
+++ comp_unit/run-docker.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+set -e
+
+if [ ! -f builtin-objs.db ]; then
+  cat ../driver_model/out/builtin-objs.db | sed 's/^\/.*linux-[0-9]\+\.[0-9]\+\.[0-9]\+\///' > builtin-objs.db
+fi
 
 docker run -it --rm -v $PWD:/mnt -u $(id -u):$(id -g) hacksaw:0.1 sh -c "cd /mnt && ./test-comp-unit.sh"
--- comp_unit/test-comp-unit.sh
+++ comp_unit/test-comp-unit.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-set -x
 set -e
 
-KERNEL_VER="5.19.17"
-KERNEL_PATH="linux-${KERNEL_VER}/"
-KERNEL_CONF="dotconfig"
+BUILD_PATH="build/"
 OUTPUT_PATH="out/"
+
+KERNEL_VER="5.19.17"
+KERNEL_PATH="$BUILD_PATH/linux-${KERNEL_VER}/"
+KERNEL_CONF="dotconfig"
+BUILTIN_OBJS="builtin-objs.db"
 OBJ_KCONF_DB="${OUTPUT_PATH}/obj-kconf.db"
 BUILD_DEP_OUT="${OUTPUT_PATH}/build-dep.out"
-OBJ_DEP="${OUTPUT_PATH}/objects.dep"
+BUILTIN_DEP="${OUTPUT_PATH}/builtin-objs.dep"
 
-if [ ! -d "${KERNEL_PATH}" ]
-then
+mkdir -p ${BUILD_PATH}
+mkdir -p ${OUTPUT_PATH}
+
+if [ ! -d "${KERNEL_PATH}" ]; then
+  pushd $BUILD_PATH
   wget -c https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${KERNEL_VER}.tar.xz -O - | tar -xJ
+  popd
 fi
 
 if [ ! -f "${KERNEL_CONF}" ]
@@ -23,8 +29,6 @@ then
   cp ${KERNEL_PATH}/.config $KERNEL_CONF
 fi
 
-mkdir -p ${OUTPUT_PATH}
-
 ./get-obj-kconf-expr.py -k ${KERNEL_PATH} -o ${OUTPUT_PATH} -n $(nproc) > ${OBJ_KCONF_DB}
-./get-build-deps.py -f ${OBJ_KCONF_DB} -c ${KERNEL_CONF} -n $(nproc) > ${BUILD_DEP_OUT}
-cat ${BUILD_DEP_OUT} | ./gen-dep.py | sort > ${OBJ_DEP}
+./get-build-deps.py -f ${OBJ_KCONF_DB} -c ${KERNEL_CONF} -b ${BUILTIN_OBJS} -n $(nproc) > ${BUILD_DEP_OUT}
+cat ${BUILD_DEP_OUT} | ./gen-dep.py | sort | uniq > ${BUILTIN_DEP}
