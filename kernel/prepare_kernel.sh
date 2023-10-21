dcbf6756d29f1010116507ba67f2d3a5fcedc206
Sangho Lee <sangho.lee.cs@gmail.com>
2023-10-21 09:24:49 -0700

    fix: create oputput dirs

--- kernel/prepare_kernel.sh
+++ kernel/prepare_kernel.sh
@@ -10,6 +10,7 @@ CURDIR=$(dirname $(realpath $0))
 ROOTDIR=$(dirname ${CURDIR})
 SRCDIR="$CURDIR/src"
 BUILDDIR="${ROOTDIR}/build/"
+OUTPUT_PATH="${ROOTDIR}/out/${KERNEL_VER}"
 
 KERNEL_SRC_PATH="$SRCDIR/linux-$KERNEL_VER/"
 KERNEL_BUILD_PATH="$BUILDDIR/linux-$KERNEL_VER/"
@@ -18,6 +19,7 @@ KERNEL_CONF_FRAGMENT="${CURDIR}/hacksaw.kconfig.fragment"
 
 mkdir -p $SRCDIR
 mkdir -p $KERNEL_BUILD_PATH
+mkdir -p $OUTPUT_PATH
 
 if [ ! -d "$KERNEL_SRC_PATH" ]; then
   wget -c https://cdn.kernel.org/pub/linux/kernel/v${KERNEL_VER:0:1}.x/linux-$KERNEL_VER.tar.xz -O - | tar -xJ -C $SRCDIR
