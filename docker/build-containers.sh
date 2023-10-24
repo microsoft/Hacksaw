347fdd9f6e2743c9d527b015d856a923e730d50c
Sangho Lee <Sangho.Lee@microsoft.com>
2023-10-24 01:47:06 +0000

    fix dockerfiles

--- docker/build-containers.sh
+++ docker/build-containers.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-UID=$(id -u)
-GID=$(id -g)
+USERID=$(id -u)
+GROUPID=$(id -g)
 
 pushd builder
-docker buildx build -f Dockerfile -t hacksaw-builder:0.1 --build-arg USER_ID=$UID --build-arg GROUP_ID=$GID .
+docker buildx build -f Dockerfile -t hacksaw-builder:0.1 --build-arg USER_ID=$USERID --build-arg GROUP_ID=$GROUPID .
 popd
 
 pushd patcher
-docker buildx build -f Dockerfile -t hacksaw-patcher:0.1 --build-arg USER_ID=$UID --build-arg GROUP_ID=$GID .
+docker buildx build -f Dockerfile -t hacksaw-patcher:0.1 --build-arg USER_ID=$USERID --build-arg GROUP_ID=$GROUPID .
 popd
